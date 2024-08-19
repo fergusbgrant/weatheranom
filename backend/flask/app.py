60a730d3cfce119f9ff5a31a193a4735baa6b3ad
Fergus Grant <fgrant@MacBook-Air.local>
2024-08-19 19:46:54 +0100

    Implementing basic user registration route

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -1,8 +1,9 @@
 import awsgi
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import json
 import mysql.connector
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/', methods=['GET'])
 @cross_origin()
-def main():
+def index():
     # Connect to DB
     cnx = mysql.connector.connect(**credentials())
     cursor = cnx.cursor()
@@ -44,6 +45,40 @@ def main():
         'responseCode': 200,
         'body': data
     })
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.json['username']
+        password = generate_password_hash(request.json['password'])
+
+        cnx = mysql.connector.connect(**credentials())
+        cursor = cnx.cursor()
+
+        sql = f"""
+            INSERT INTO USERS (USERNAME, PASSHASH) VALUES ('{username}','{password}')
+        """
+
+        cursor.execute(sql)
+        cnx.commit()
+
+        cursor.close()
+        cnx.close()
+
+        return jsonify({
+            "responseCode": 200,
+            "body": {
+                "username": username,
+                "hash": password
+            }
+        })
+    
+    elif request.method == 'GET':
+        return jsonify({
+            'responseCode': 200,
+            'body': 'poo'
+        })
 
 
 def credentials():
